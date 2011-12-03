76713abe8f6862a2b185a80a44b09e47f06fd920
Micah Pearlman <micahpearlman@gmail.com>
2011-12-02 17:04:59 -0800

    initial commit

---
+++ src/zoJSON.cpp
@@ -0,0 +1,363 @@
+#include "zoJSON.h"
+#include <cctype>
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+
+using namespace std;
+namespace zo {
+
+// Try to consume characters from the input stream and match the
+// pattern string.
+bool match(const char* pattern, std::istream& input) {
+    input >> std::ws;
+    const char* cur(pattern);
+    char ch(0);
+    while(input && !input.eof() && *cur != 0) {
+        input.get(ch);
+        if (ch != *cur) {
+            input.putback(ch);
+            return false;
+        } else {
+            cur++;
+        }
+    }
+    return *cur == 0;
+}
+
+bool parse_string(std::istream& input, std::string* value) {
+    if (!match("\"", input))  {
+        return false;
+    }
+    char ch;
+    while(!input.eof() && input.good()) {
+        input.get(ch);
+        if (ch == '"') {
+            break;
+        }
+        if (ch == '\\') {
+            input.get(ch);
+            switch(ch) {
+                case '"':
+                case '\\':
+                case '/':
+                    value->push_back(ch);
+                    break;
+                case 'b':
+                    value->push_back('\b');
+                    break;
+                case 'f':
+                    value->push_back('\f');
+                    break;
+                case 'n':
+                    value->push_back('\n');
+                    break;
+                case 'r':
+                    value->push_back('\r');
+                    break;
+                case 't':
+                    value->push_back('\t');
+                    break;
+                default:
+                    value->push_back('\\');
+                    value->push_back(ch);
+                    break;
+            }
+        } else {
+            value->push_back(ch);
+        }
+    }
+    if (input && ch == '"') {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool parse_bool(std::istream& input, bool* value) {
+    if (match("true", input))  {
+        *value = true;
+        return true;
+    }
+    if (match("false", input)) {
+        *value = false;
+        return true;
+    }
+    return false;
+}
+
+bool parse_null(std::istream& input) {
+    if (match("null", input))  {
+        return true;
+    }
+    return false;
+}
+
+bool parse_number(std::istream& input, double* value) {
+    input >> std::ws;
+    input >> *value;
+    if (input.fail()) {
+        input.clear();
+        return false;
+    }
+    return true;
+}
+
+Object::Object() : _value_map() {}
+
+Object::~Object() {
+//    std::map<std::string, Value>::iterator i;
+//    for (i = _value_map.begin(); i != _value_map.end(); ++i) {
+//        delete i->second;
+//    }
+}
+
+bool Object::parse(std::istream& input, Object& object) {
+    object._value_map.clear();
+
+    if (!match("{", input)) {
+        return false;
+    }
+
+    do {
+        std::string key;
+        if (!parse_string(input, &key)) {
+            return false;
+        }
+        if (!match(":", input)) {
+            return false;
+        }
+        Value v;
+        if (!Value::parse(input, v)) {
+            break;
+        }
+        object._value_map[key] = v;
+    } while (match(",", input));
+
+    if (!match("}", input)) {
+        return false;
+    }
+    return true;
+}
+	
+	void Object::describe() const {
+		std::map<std::string, Value>::const_iterator it;
+		for ( it = _value_map.begin(); it != _value_map.end(); it++ ) {
+			cout << "\tkey: " << it->first << endl;
+			it->second.describe();
+		}
+	}
+
+
+Value::Value() : _type(kTypeInvalid) {}
+
+void Value::reset() {
+	_value = boost::any();
+}
+
+bool Value::parse(std::istream& input, Value& value) {
+    value.reset();
+
+    std::string string_value;
+    if (parse_string(input, &string_value)) {
+        value._value = string_value;
+        value._type = kTypeString;
+        return true;
+    }
+	double double_value;
+    if (parse_number(input, &double_value)) {
+		value._value = double_value;
+        value._type = kTypeNumber;
+        return true;
+    }
+
+	bool bool_value;
+    if (parse_bool(input, &bool_value)) {
+		value._value = bool_value;
+        value._type = kTypeBool;
+        return true;
+    }
+    if (parse_null(input)) {
+        value._type = kTypeNull;
+        return true;
+    }
+    if (input.peek() == '[') {
+		Array array_value;
+        
+        if (Array::parse(input, array_value)) {
+			value._value = array_value;
+            value._type = kTypeArray;
+            return true;
+        }
+    }
+
+	Object object_value;
+    if (Object::parse(input, object_value)) {
+		value._value = object_value;
+        value._type = kTypeObject;
+        return true;
+    }
+    return false;
+}
+	
+	void Value::describe() const {
+		switch (_type) {
+			case kTypeNumber:
+				std::cout << "number: " << get<double>() << std::endl; 
+				break;
+			case kTypeString:
+				std::cout << "string: " << get<std::string>() << std::endl; 
+				break;
+			case kTypeBool:
+				std::cout << "bool: " << get<bool>() << std::endl; 
+				break;
+			case kTypeNull:
+				std::cout << "null: " << std::endl; 
+				break;
+			case kTypeArray:
+				std::cout << "array: " << std::endl; 
+				get<Array>().describe();
+				break;
+			case kTypeObject:
+				std::cout << "object: " << std::endl; 
+				get<Object>().describe();
+				break;
+				
+				
+			default:
+				break;
+		}
+	}
+	
+
+Array::Array() : _values() {}
+	Array::Array( int sz ) 
+	:	_values()
+	{
+		reserve( sz );
+	}
+
+Array::~Array() {
+//    for (unsigned int i = 0; i < _values.size(); ++i) {
+//        delete _values[i];
+//    }
+}
+
+bool Array::parse(std::istream& input, Array& array) {
+    array._values.clear();
+
+    if (!match("[", input)) {
+        return false;
+    }
+
+    do {
+        Value v;
+        if (!Value::parse(input, v)) {
+            break;
+        }
+        array._values.push_back(v);
+    } while (match(",", input));
+
+    if (!match("]", input)) {
+        return false;
+    }
+    return true;
+}
+	
+	void Array::describe() const {
+		for (unsigned int i = 0; i < _values.size(); ++i) {
+			_values[i].describe();
+		}
+	}
+
+static std::ostream& stream_string(std::ostream& stream,
+                                   const std::string& string) {
+    stream << '"';
+    for (std::string::const_iterator i = string.begin(),
+                 e = string.end(); i != e; ++i) {
+        switch (*i) {
+            case '"':
+                stream << "\\\"";
+                break;
+            case '\\':
+                stream << "\\\\";
+                break;
+            case '/':
+                stream << "\\/";
+                break;
+            case '\b':
+                stream << "\\b";
+                break;
+            case '\f':
+                stream << "\\f";
+                break;
+            case '\n':
+                stream << "\\n";
+                break;
+            case '\r':
+                stream << "\\r";
+                break;
+            case '\t':
+                stream << "\\t";
+                break;
+            default:
+                if (*i < 32) {
+                    stream << "\\u" << std::hex << std::setw(6) <<
+                            std::setfill('0') << static_cast<int>(*i) << std::dec <<
+                            std::setw(0);
+                } else {
+                    stream << *i;
+                }
+        }
+    }
+    stream << '"';
+    return stream;
+}
+
+}  // namespace jsonxx
+
+std::ostream& operator<<(std::ostream& stream, const zo::Value& v) {
+    if (v.is<double>()) {
+        return stream << v.get<double>();
+    } else if (v.is<std::string>()) {
+        return zo::stream_string(stream, v.get<std::string>());
+    } else if (v.is<bool>()) {
+        if (v.get<bool>()) {
+            return stream << "true";
+        } else {
+            return stream << "false";
+        }
+    } else if (v.is<zo::Value::Null>()) {
+        return stream << "null";
+    } else if (v.is<zo::Object>()) {
+        return stream << v.get<zo::Object>();
+    } else if (v.is<zo::Array>()){
+        return stream << v.get<zo::Array>();
+    }
+    // Shouldn't reach here.
+    return stream;
+}
+
+std::ostream& operator<<(std::ostream& stream, const zo::Array& v) {
+    stream << "[";
+    const std::vector<zo::Value>& values(v.values());
+    for (unsigned int i = 0; i < values.size()-1; ++i) {
+        stream << values[i] << ", ";
+    }
+    return stream << values[values.size()-1] << "]";
+}
+
+std::ostream& operator<<(std::ostream& stream, const zo::Object& v) {
+    stream << "{";
+    const std::map<std::string, zo::Value>& kv(v.kv_map());
+    for (std::map<std::string, zo::Value>::const_iterator i = kv.begin();
+         i != kv.end(); /**/) {
+        zo::stream_string(stream, i->first);
+        stream << ": " << i->second;
+        ++i;
+        if ( i != kv.end()) {
+            stream << ", ";
+        }
+    }
+    return stream << "}";
+}
---
+++ src/zoJSON.h
@@ -0,0 +1,418 @@
+//
+//  zoJSON.h
+//
+//  Created by Micah Pearlman on 5/30/11.
+//  Copyright 2011 Zero Engineering. All rights reserved.
+//
+// Heavily based off jsonxx: 
+// Author: Hong Jiang <hong@hjiang.net>
+// Contributors:
+//   Sean Middleditch <sean@middleditch.us>
+
+#ifndef _jsonxx_h__
+#define _jsonxx_h__
+
+#include <cassert>
+#include <iostream>
+#include <map>
+#include <vector>
+#include <boost/any.hpp>
+
+namespace zo {
+
+	// A value could be a number, an array, a string, an object, a
+	// boolean, or null
+	class Array;
+	class Object;
+	
+	class Value {
+	public:
+		class Null {};
+		
+		Value();
+		Value( const char* c ) {
+			_value = std::string(c);
+			_type = kTypeString;
+		}
+		Value( const std::string& s ) {
+			_value = s;
+			_type = kTypeString;
+		}
+		Value( double d ) {
+			_value = d;
+			_type = kTypeNumber;
+		}
+		Value( bool b ) {
+			_value = b;
+			_type = kTypeBool;
+		}
+		Value( const Object& o ) {
+			_value = o;
+			_type = kTypeObject;
+		}
+		Value( const Array& a ) {
+			_value = a;
+			_type = kTypeArray;
+		}
+		
+
+		~Value() { reset(); }
+		void reset();
+		
+		static bool parse(std::istream& input, Value& value);
+		
+		template<typename T> bool is() const;
+		template<typename T> T& get();
+		template<typename T> const T& get() const;
+
+		Value(const Value& o ) {
+			_value = o._value;
+			_type = o._type;
+		}
+		
+		Value& operator=(const Value& o) {
+			_value = o._value;
+			_type = o._type;
+			return *this;
+		}
+		
+		Value& operator[]( int i ); 
+		const Value& operator[]( int i ) const; 
+		
+		Value& operator[]( const char* k );
+		const Value& operator[]( const char* k ) const;
+		
+		Value& operator[]( const std::string& k );
+		const Value& operator[]( const std::string& k ) const;
+		
+		inline bool has( const std::string& key ) const;
+		
+
+		
+		void describe() const;
+		
+	private:
+		
+		
+		enum {
+			kTypeNumber = 0,
+			kTypeString = 1,
+			kTypeBool = 2,
+			kTypeNull = 3,
+			kTypeArray = 4,
+			kTypeObject = 5,
+			kTypeInvalid = 6
+		} _type;
+		
+		boost::any		_value;
+	};
+
+
+	// A JSON Object
+	class Object {
+	public:
+		Object();
+		~Object();
+
+		static bool parse(std::istream& input, Object& object);
+
+		template <typename T> bool hasType(const std::string& key) const;
+		inline bool has( const std::string& key ) const;
+
+		// Always call has<>() first. If the key doesn't exist, consider
+		// the behavior undefined.
+		template <typename T> T& get(const std::string& key);
+		template <typename T> const T& get(const std::string& key) const;
+		
+		void add( const std::string& key, const Value& v ) {
+			_value_map[key] = Value( v );
+		}
+
+		const Value& value(const std::string& key) const {return _value_map.find(key)->second;}	
+		Value& value(const std::string& key) {return _value_map.find(key)->second;}	
+
+		const std::map<std::string, Value>& kv_map() const { return _value_map; }
+		
+		Object( const Object& o ) { _value_map = o._value_map; }
+		Object& operator=( const Object& o ) { _value_map = o._value_map; return *this; }
+		
+		Value& operator[]( const std::string& k ) { 
+			std::map<std::string, Value>::iterator it = _value_map.find(k);
+			if ( it != _value_map.end() ) {
+				return it->second;
+			}
+			// else we create a NULL value
+			add( k, Value() );
+			it = _value_map.find(k);
+			return it->second; 
+		}
+		const Value& operator[]( const std::string& k ) const { return value(k); }
+
+		
+		void describe() const;
+
+	private:
+
+		std::map<std::string, Value> _value_map;
+	};
+
+	class Array {
+	public:
+		Array();
+		Array( int sz );
+		~Array();
+
+		static bool parse(std::istream& input, Array& array);
+
+		unsigned int size() const { return _values.size(); }
+
+		template <typename T> bool has(unsigned int i) const;
+
+		template <typename T> T& get(unsigned int i);
+		template <typename T> const T& get(unsigned int i) const;
+		const Value& operator[]( int i ) const { return value( i ); }
+		Value& operator[]( int i ) { return value( i ); }
+
+		const Value& value( unsigned int i ) const { return _values[i]; }
+		Value& value( unsigned int i ) {
+			if( i >= size() )
+				reserve( i + 1 );
+			return _values[i];
+		}
+
+		const std::vector<Value>& values() const {
+			return _values;
+		}
+		
+		void reserve( int sz ) {
+			for( int i = size(); i < sz; i++ ) {
+				_values.push_back( Value() );
+			}
+		}
+
+		Array(const Array& o) { _values = o._values; }
+		Array& operator=(const Array& o) { _values = o._values; return *this; }
+		
+		void describe() const;
+
+	private:
+		std::vector<Value> _values;
+	};
+
+
+	template <typename T>
+	bool Array::has(unsigned int i) const {
+		if (i >= size()) {
+			return false;
+		} else {
+			const Value& v = _values.at(i);
+			return v.is<T>();
+		}
+	}
+
+	template <typename T>
+	T& Array::get(unsigned int i) {
+		assert(i < size());
+		Value& v = _values.at(i);
+		return v.get<T>();
+	}
+
+	template <typename T>
+	const T& Array::get(unsigned int i) const {
+		assert(i < size());
+		const Value& v = _values.at(i);
+		return v.get<T>();
+	}
+	
+	inline bool Object::has( const std::string& key ) const {
+		std::map<std::string, Value>::const_iterator it(_value_map.find(key));
+		return it != _value_map.end();
+	}
+
+	template <typename T>
+	bool Object::hasType(const std::string& key) const {
+		std::map<std::string, Value>::const_iterator it(_value_map.find(key));
+		return it != _value_map.end() && it->second.is<T>();
+	}
+
+	template <typename T>
+	T& Object::get(const std::string& key) {
+	  assert(hasType<T>(key));
+	  return _value_map.find(key)->second.get<T>();
+	}
+
+	template <typename T>
+	const T& Object::get(const std::string& key) const {
+	  assert(hasType<T>(key));
+	  return _value_map.find(key)->second.get<T>();
+	}
+
+	template<>
+	inline bool Value::is<Value::Null>() const {
+	  return _type == kTypeNull;
+	}
+
+	template<>
+	inline bool Value::is<bool>() const {
+	  return _type == kTypeBool;
+	}
+
+	template<>
+	inline bool Value::is<std::string>() const {
+	  return _type == kTypeString;
+	}
+
+	template<>
+	inline bool Value::is<double>() const {
+	  return _type == kTypeNumber;
+	}
+
+	
+	template<>
+	inline bool Value::is<Array>() const {
+	  return _type == kTypeArray;
+	}
+
+	template<>
+	inline bool Value::is<Object>() const {
+	  return _type == kTypeObject;
+	}
+
+	template<>
+	inline bool& Value::get<bool>() {
+		assert(is<bool>());
+		bool* b = boost::any_cast<bool>(&_value);
+		return *b;
+	}
+
+	template<>
+	inline std::string& Value::get<std::string>() {
+		assert(is<std::string>());
+		std::string* s = boost::any_cast<std::string>(&_value);
+		return *s;
+	}
+
+	template<>
+	inline double& Value::get<double>() {
+		assert(is<double>());
+		double* d = boost::any_cast<double>(&_value);
+		return *d;
+	}
+
+
+	template<>
+	inline Array& Value::get<Array>() {
+		assert(is<Array>());
+		Array* a = boost::any_cast<Array>(&_value);
+		return *a;
+	}
+
+	template<>
+	inline Object& Value::get<Object>() {
+		assert(is<Object>());
+		Object* o = boost::any_cast<Object>(&_value);
+		return *o;
+	}
+
+	template<>
+	inline const bool& Value::get<bool>() const {
+		assert(is<bool>());
+		const bool* b = boost::any_cast<bool>(&_value); 
+		return *b;
+	}
+
+	template<>
+	inline const std::string& Value::get<std::string>() const {
+	  assert(is<std::string>());
+		const std::string* s = boost::any_cast<std::string>(&_value);
+		return *s;
+	}
+
+	template<>
+	inline const double& Value::get<double>() const {
+		assert(is<double>());
+		const double* d = boost::any_cast<double>(&_value);
+		return *d;
+	}
+
+
+	template<>
+	inline const Array& Value::get<Array>() const {
+		assert(is<Array>());
+		const Array* a = boost::any_cast<Array>(&_value);
+		return *a;
+	}
+
+	template<>
+	inline const Object& Value::get<Object>() const {
+		assert(is<Object>());
+		const Object* o = boost::any_cast<Object>(&_value);
+		return *o;
+	}
+
+	
+	inline Value& Value::operator[]( int i ) {
+		return get<Array>().value( i );
+	}
+	inline const Value& Value::operator[]( int i ) const {
+		return get<Array>().value( i );
+	}
+	
+	inline Value& Value::operator[]( const char* k ) {
+		return get<Object>().value( std::string(k) );
+	}
+	inline const Value& Value::operator[]( const char* k ) const {
+		return get<Object>().value( std::string(k) );
+	}
+	
+	inline Value& Value::operator[]( const std::string& k ) {
+		return get<Object>().value( k );
+	}
+	inline const Value& Value::operator[]( const std::string& k ) const {
+		return get<Object>().value( k );
+	}
+
+	inline bool Value::has( const std::string& key ) const {
+		return get<Object>().has( key );
+	}
+	
+	inline std::string& operator<<(std::string& s, const zo::Value& v ) {
+		s = v.get<std::string>();
+		return s;
+	}
+	
+	inline double& operator<<(double& s, const zo::Value& v ) {
+		s = v.get<double>();
+		return s;
+	}
+	
+	inline Array& operator<<(Array& s, const zo::Value& v ) {
+		s = v.get<Array>();
+		return s;
+	}
+	
+	inline Object& operator<<(Object& s, const zo::Value& v ) {
+		s = v.get<Object>();
+		return s;
+	}
+
+	inline bool& operator<<(bool& s, const zo::Value& v ) {
+		s = v.get<bool>();
+		return s;
+	}
+
+	inline float& operator<<(float& s, const zo::Value& v ) {
+		s = v.get<double>();
+		return s;
+	}
+
+
+}  // namespace jsonxx
+
+	std::ostream& operator<<(std::ostream& stream, const zo::Value& v);
+	std::ostream& operator<<(std::ostream& stream, const zo::Object& v);
+	std::ostream& operator<<(std::ostream& stream, const zo::Array& v);
+
+#endif // _jsonxx_h__
+
+
